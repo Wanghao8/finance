9d748b8cabdc133637f2b83a4994bc920634ebf8
ZZPC-023\ZZPC-023 <1415474337@qq.com>
2020-08-26 18:34:29 +0800

    刚创建项目

---
+++ pages/index/index.js
@@ -0,0 +1,42 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        active: 0,
+        icon: {
+            index: '../../assets/images/home-2.png',
+            indexactive: '../../assets/images/home.png',
+            inout: '../../assets/images/detail.png',
+            inoutactive: '../../assets/images/detail-2.png',
+            set: '../../assets/images/settings.png',
+            setactive: '../../assets/images/settings-2.png',
+        },
+    },
+    onChange(event) {
+        switch (event.detail) {
+            case 0:
+                wx.switchTab({
+                    url: '../index/index',
+                })
+                break;
+            case 1:
+                wx.switchTab({
+                    url: '../inoutdetail/inoutdetail',
+                })
+                break;
+            case 2:
+                wx.switchTab({
+                    url: '../setting/setting',
+                })
+                break;
+
+            default:
+                break;
+        }
+    },
+    onLoad: function() {
+
+    },
+})
---
+++ pages/setting/setting.js
@@ -0,0 +1,95 @@
+// pages/setting/setting.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        active: 2,
+        icon: {
+            index: '../../assets/images/home-2.png',
+            indexactive: '../../assets/images/home.png',
+            inout: '../../assets/images/detail.png',
+            inoutactive: '../../assets/images/detail-2.png',
+            set: '../../assets/images/settings.png',
+            setactive: '../../assets/images/settings-2.png',
+        },
+    },
+    onChange(event) {
+        switch (event.detail) {
+            case 0:
+                wx.switchTab({
+                    url: '../index/index',
+                })
+                break;
+            case 1:
+                wx.switchTab({
+                    url: '../inoutdetail/inoutdetail',
+                })
+                break;
+            case 2:
+                wx.switchTab({
+                    url: '../setting/setting',
+                })
+                break;
+
+            default:
+                break;
+        }
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
